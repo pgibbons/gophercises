5ba71fb7a7c90bb9994e4f85a72f8b3e1b63fc23
pgibbons <6475190+pgibbons@users.noreply.github.com>
2022-01-01 18:51:46 -0800

    Quiz Game part 2 complete

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 var helpFlag bool
@@ -37,19 +38,31 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println(fmt.Sprintf("Starting Quiz Game with args: %s File with a time limit of %d Seconds", csvFlag, limitFlag))
+	fmt.Printf("Starting Quiz Game with args: %s File with a time limit of %d Seconds\n", csvFlag, limitFlag)
 
 	game_records := readCsvFile(csvFlag)
 	correct := 0
+
+	timer := time.NewTimer(time.Duration(limitFlag) * time.Second)
+
 	for _, record := range game_records {
-		reader := bufio.NewReader(os.Stdin)
 		fmt.Print(fmt.Sprintf("%s: ", record[0]))
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
-		if text == record[1] {
-			correct += 1
+		answerChan := make(chan string)
+		go func() {
+			reader := bufio.NewReader(os.Stdin)
+			text, _ := reader.ReadString('\n')
+			answerChan <- strings.TrimSpace(text)
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTotal Score: %d out of %d Correct\n", correct, len(game_records))
+			return
+		case answer := <-answerChan:
+			if answer == record[1] {
+				correct += 1
+			}
 		}
 	}
 
-	fmt.Println(fmt.Sprintf("Total Score: %d out of %d Correct", correct, len(game_records)))
+	fmt.Printf("Total Score: %d out of %d Correct\n", correct, len(game_records))
 }
